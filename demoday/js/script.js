4363efde60fcba9a05d5ca807c369c2896628125
navidmx <navidmx@gmail.com>
2017-08-12 23:17:59 -0400

    Updated demoday

--- demoday/js/script.js
+++
@@ -1,9 +0,0 @@
-//Loops images at the end
-Reveal.addEventListener('slidechanged', function (event) {
-    if (Reveal.isAutoSliding()){
-        Reveal.configure({ controls: false });
-    }
-    if (Reveal.isLastSlide()){
-        Reveal.slide(9);
-    }
-});
